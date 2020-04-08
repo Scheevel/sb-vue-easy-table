cb51fcddcdbfb6bd0fa8fa2b2fd6d97b5a00a669
Scheevel <scheevel@gmail.com>
2020-04-08 12:39:34 -0500

    last known good

--- src/main.js
+++ src/main.js
@@ -4,17 +4,33 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
-Vue.component(App.name,App)
+//Vue.component(App.name,App)
 
 function returnDV() {
   return "deepvaluereturn"
 }
+/*
+function propObj() {
+  return {deep: {
+    default: returnDV(),
+    type: String
+  }
+  }
+}
+*/
 
 new Vue({
   el: '#app',
   store,
   /*data: ()=> {return{deep:"testvalue"}},*/
   /*components: {App},*/
-  template:'<App/>',
-  render: h => h(App, {props: { deep:returnDV()}})
+  /*template:'<App/>',*/
+  render: h => h(App, { 
+    props: {
+      deep: {
+        type: String,
+        default: returnDV(),
+      }
+    }
+  })
 })//.$mount("#app");
